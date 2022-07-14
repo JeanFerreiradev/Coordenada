f45598f274c26ad85656b4d1840d1c8a995573b6
Jean WIlson Ferreira <100369102+JeanWf1213@users.noreply.github.com>
2022-07-14 13:10:25 -0300

    Update Coordenada.java

--- Coordenada.java
+++ Coordenada.java
@@ -5,8 +5,6 @@ import java.text.DecimalFormat;
 
 public class Coordenada {
 	static Scanner console = new Scanner(System.in);
-	static DecimalFormat formatador = new DecimalFormat("0.0");
-
 	public static void main(String[] args) {
 		double x = 0.0;
 		double y = 0.0;
@@ -15,19 +13,19 @@ public class Coordenada {
 		System.out.println("Digite a coordenada Y:");
 		y = console.nextDouble();
 		if (x == 0 && y == 0) {
-			System.out.println("\tO ponto est· na origem do plano!");
+			System.out.println("\tO ponto est√° na origem do plano!");
 		} else if (x > 0 && y == 0) {
-			System.out.println("\tO ponto est· sobre o eixo X!");
+			System.out.println("\tO ponto est√° sobre o eixo X!");
 		} else if (x == 0 && y > 0) {
-			System.out.println("\tO ponto est· sobre o eixo Y!");
+			System.out.println("\tO ponto est√° sobre o eixo Y!");
 		} else if (x > 0 && y > 0) {
-			System.out.println("\tO ponto est· no quadrante 1!");
+			System.out.println("\tO ponto est√° no quadrante 1!");
 		} else if (x > 0 && y < 0) {
-			System.out.println("\tO ponto est· no quadrante 4!");
+			System.out.println("\tO ponto est√° no quadrante 4!");
 		} else if (x < 0 && y > 0) {
-			System.out.println("\tO ponto est· no quadrante 2!");
+			System.out.println("\tO ponto est√° no quadrante 2!");
 		} else {
-			System.out.println("\tO ponto est· no quadrante 3!");
+			System.out.println("\tO ponto est√° no quadrante 3!");
 		}
 
 	}
